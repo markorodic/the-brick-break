43c7f5359496bbdff99435eb054a22b2da570dec
Paul Dariye <paul.dariye@gmail.com>
2019-01-20 21:33:03 -0500

    exploration

---
+++ README.md
@@ -0,0 +1,25 @@
+# Breakout Game
+
+## Profiling
+
+- Game performance
+- Code performance
+
+
+Understanding how each implementation is compiled and how that affects
+performance.
+
+Things to measure?
+
+- game loop?
+- update function?
+- render function?
+
+
+
+TODO:
+
+- [] move implementations of game to repo
+- [] run instance of each game inside a puppeteer
+- [] print the results and see what sticks out
+- [] post on zulip
---
+++ profile.js
@@ -0,0 +1,32 @@
+/*
+ *
+ *
+ * 1. start game server
+ * 2. pass in url of game server to puppeteer
+ * 3. output metrics to cli
+ */
+
+// const server = require('./server')
+
+const puppeteer = require('puppeteer')
+
+async function run () {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000');
+
+  console.log("\n==== performance.getEntries() ====\n");
+  console.log( await page.evaluate( () => JSON.stringify(performance.getEntries(), null, "  ") ) );
+
+  console.log("\n==== performance.toJSON() ====\n");
+  console.log( await page.evaluate( () => JSON.stringify(performance.toJSON(), null, "  ") ) );
+
+  console.log("\n==== page.metrics() ====\n");
+  const perf = await page.metrics();
+  console.log( JSON.stringify(perf, null, "  ") );
+
+  // browser.close();
+}
+
+run()
+
