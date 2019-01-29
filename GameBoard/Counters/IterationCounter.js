826d1d544efad48684c70ef9f22600277d5d9c6b
markorodic <mrmarkorodic@googlemail.com>
2019-01-29 16:24:07 -0500

    Add tests for logging the data from each game

--- GameBoard/Counters/IterationCounter.js
+++ GameBoard/Counters/IterationCounter.js
@@ -1,17 +1,43 @@
+const REPORT_TEST = true;
+let NUMBER_OF_ITERATORS = 2;
+let iteratorCounter = 0;
+
 export class IterationCounter {
-  constructor(body) {
+  constructor(name) {
+    this.frameCount = 0;
+    this.frameLog = [];
     this.frames = [];
     this.startTime = 0;
     this.EndTime = 0;
     this.delta = 0;
+    this.name = name;
   }
-  startCalc(body) {
+  startCalc() {
     this.startTime = performance.now();
   }
-  endCalc(body) {
+  endCalc(name) {
     this.endTime = performance.now();
     const delta = this.endTime - this.startTime;
     this.frames.push(delta);
+    if (REPORT_TEST) {
+      if (this.frameCount < 2000) {
+        this.frameCount++;
+        this.frameLog.push({
+          startTime: this.startTime,
+          endTime: this.endTime,
+          delta
+        });
+      } else if (this.frameCount === 2000) {
+        this.frameCount++;
+        window.DO_THA_REPORT_PLEASE(this.frameLog, this.name);
+        if (
+          ++iteratorCounter >=
+          NUMBER_OF_ITERATORS
+        ) {
+          window.PLEASE_CLOSE_ME()
+        }
+      }
+    }
     if (this.frames.length > 10) {
       this.frames.shift();
     }
---
+++ system.test.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer')
+
+jest.setTimeout(60 * 1000)
+
+describe('the system', () => {
+  let browser
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false
+    })
+  })
+  
+  afterAll(async () => {
+    if (browser) {
+      await browser.close()
+    }
+  })
+
+  it('should produce reports', async () => {
+    const page = await browser.newPage()
+
+    await page.exposeFunction('DO_THA_REPORT_PLEASE', async (data) => {
+      console.log('got data', data.length)
+      const dataSum = data.reduce((sum, datum, index) => {
+        return sum + datum.delta
+      }, 0)
+      const dataMean = dataSum / data.length
+      console.log(dataMean);
+    })
+
+    const deferred = waitForClose(page);
+    await page.goto('http://localhost:3000')
+    await deferred
+  });
+
+  it('should produce more reports', async () => {
+    const page = await browser.newPage()
+
+    await page.exposeFunction('DO_THA_REPORT_PLEASE', async (data) => {
+      console.log('got data for second test', data.length)
+      // testing continues here
+    })
+
+    const deferred = waitForClose(page);
+    await page.goto('http://localhost:3000')
+    await deferred
+  });
+
+  function waitToClose(page) {
+    return new Promise((resolve, reject) => {
+      page.on('close', resolve)
+      page.on('error', reject)
+      setTimeout(() => reject(new Error('timed out waiting for page to close itself')), 30 * 1000)
+    });
+  }
+
+  function waitForClose(page) {
+    return new Promise((resolve, reject) => {
+      //page.on('close', resolve)
+      page.exposeFunction('PLEASE_CLOSE_ME', () => {
+        resolve()
+      })
+      page.on('error', reject)
+      setTimeout(() => reject(new Error('timed out waiting for page to close itself')), 30 * 1000)
+    });
+  }
+});
