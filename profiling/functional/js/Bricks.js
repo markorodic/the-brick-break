873038311e1ec52c510835054d589398d64e3fc6
markorodic <mrmarkorodic@googlemail.com>
2019-02-02 15:30:14 -0500

    Add profiling functial test game

--- profiling/functional/js/Bricks.js
+++ profiling/functional/js/Bricks.js
@@ -12,7 +12,7 @@ function assignBrickPositions(emptyBrickList) {
     var yMultiple = Math.floor(index / 14);
     var x = 29 * xMultiple + 46.5;
     var y = 29 * yMultiple + 46.5;
-    var colour = whichColour(yMultiple, "red");
+    var colour = whichColour(yMultiple);
     return { x: x, y: y, colour };
   });
   return assignedBrickList;
